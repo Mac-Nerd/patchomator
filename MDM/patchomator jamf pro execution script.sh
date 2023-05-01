84fd06f75d40ee6b0586f5234b6eeff29a8d3687
Charles Mangin - Mac Nerd <95777330+Mac-Nerd@users.noreply.github.com>
2023-05-01 14:46:17 -0400

    minor script cleanup

--- MDM/patchomator jamf pro execution script.sh
+++ MDM/patchomator jamf pro execution script.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 #
-# This script will check if Installomator and Patchomtor are found locally in their folder. 
-# If found it will execute Patchomator silently (--yes) to install (--install) updates if found.
-# Jamf Pro script parameter 4 is used to pass on ignored labels (space separated) and parameter 5 to pass on required labels (space separated).
-# This could be adapted to replace triggers in bottom of the script by more Jamf Pro script parameters to have more control. 
-#
+# This script will check if Installomator and Patchomtor are found locally in their respecitve folders. 
+# If found, it will execute Patchomator silently (--yes) to install (--install) updates if found.
+
+# Jamf Pro script parameters 
+# $4 = ignored labels (space separated) 
+# $5 = required labels (space separated).
+
+# This could be adapted to replace triggers at the end of the script by more Jamf Pro script parameters to have more control. 
+
 InstallomatorPath=("/usr/local/Installomator/Installomator.sh")
 PatchomatorPath=("/usr/local/Installomator/patchomator.sh")
 #
@@ -12,27 +16,30 @@ PatchomatorPath=("/usr/local/Installomator/patchomator.sh")
 #
 if [ ! -f $InstallomatorPath ]
 then 
-echo Installomator is not installed. Exitting...
-exit 1
+	echo "Installomator is not installed. Exiting."
+	exit 1
 fi
-echo Installomator is installed. Continuing...
+echo "Installomator is installed. Continuing."
+
 #
 # Check if Patchomator is found locally.
 #
 if [ ! -f $PatchomatorPath ]
 then 
-	echo Patchomator is not installed. Exitting...
+	echo "Patchomator is not installed. Exiting."
 	exit 2
 fi
-echo Patchomator is installed. Continuing...
+echo "Patchomator is installed. Continuing."
+
 #
 # Run Patchomator silently with optional exclusions and requirements.
 # This uses Jamf Pro script parameter 4 for ignored labels and parameter 5 for required labels.
 #
-echo Running Patchomator with triggers --yes --install --ignored --required...
+echo "Running \"patchomator.sh --yes --install --ignored --required\""
 zsh "$PatchomatorPath" --yes --install --ignored "$4" --required "$5"
-echo Patchomator script has finished. Exitting...
-exit
+echo "Patchomator script has finished. Exiting."
+exit 0
+
 #
 # End of file.
 #
